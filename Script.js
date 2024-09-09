1a2eebbd51ec2fe9d51ce1726da572deac4cff9d
RobsonLMDS <98341530+Robsonlmds@users.noreply.github.com>
2024-09-09 18:21:29 -0300

    Att 09/09

--- Script.js
+++ Script.js
@@ -1,44 +1,32 @@
-SetTell();
-
-function SetDdd (){
-    const ddd = window
-        .prompt("DDD: ")
-        .replaceAll("-","")
-        .replaceAll("(","")
-        .replaceAll(")","")
-        .replaceAll(" ","")
+function SetDdd() {
+    const ddd = document.getElementById("dddInput").value
+        .replaceAll("-", "")
+        .replaceAll("(", "")
+        .replaceAll(")", "")
+        .replaceAll(" ", "")
         .slice(0, 2);
 
     return ddd;
-};
+}
 
-//////////////////////////////////////////////////////////////////////////////
-
-function SetTell (){
-    const tell = window
-        .prompt("Tel: ")
-        .replaceAll("-","")
-        .replaceAll("(","")
-        .replaceAll(")","")
-        .replaceAll(" ","")
-        .slice(0, 9)
-        .replaceAll("11","");
-   
+function SetTell() {
+    const tell = document.getElementById("telInput").value
+        .replaceAll("-", "")
+        .replaceAll("(", "")
+        .replaceAll(")", "")
+        .replaceAll(" ", "")
+        .slice(0, 9);
+        
     let cutTell1 = tell.slice(0, 5);
-    let cutTell2 = tell.slice(5, 11);
-   
-    const resultTell = "(" +  SetDdd() + ") " + " " + cutTell1 + "-" + cutTell2; 
-   
+    let cutTell2 = tell.slice(5, 9);
+
+    const resultTell = "(" + SetDdd() + ") " + cutTell1 + "-" + cutTell2;
     document.getElementById("putTel").innerHTML = resultTell;
-
     return "";
-};
+}
 
-///////////////////////////////////////////////////////////////////////////////
-
-function SetName (){
-    const names = window
-        .prompt("Name: ")
+function SetName() {
+    const names = document.getElementById("nameInput").value
         .toLowerCase()
         .split(" ");
 
@@ -48,29 +36,23 @@ function SetName (){
         allName += elm.charAt(0).toUpperCase() + elm.substring(1) + " ";
     });
 
-    document.getElementById("putName").innerHTML = allName;
+    document.getElementById("putName").innerHTML = allName.trim();
     return "";
-};
+}
 
-////////////////////////////////////////////////////////////////////////////////
-
-function SetCpf(){
-    const cpf = window
-        .prompt("CPF: ")
-        .replaceAll(" ","")
-        .replaceAll(".","")
-        .replaceAll("-","");
-
+function SetCpf() {
+    const cpf = document.getElementById("cpfInput").value
+        .replaceAll(" ", "")
+        .replaceAll(".", "")
+        .replaceAll("-", "");
 
     let cutCpf1 = cpf.slice(0, 3);
     let cutCpf2 = cpf.slice(3, 6);
     let cutCpf3 = cpf.slice(6, 9);
-    let cutCpf4 = cpf.slice(9, 12);
+    let cutCpf4 = cpf.slice(9, 11);
 
-    let cpfResult = cutCpf1 + "." + cutCpf2 + "." + cutCpf3 + "-" + cutCpf4; 
+    let cpfResult = cutCpf1 + "." + cutCpf2 + "." + cutCpf3 + "-" + cutCpf4;
 
     document.getElementById("putCpf").innerHTML = cpfResult;
     return "";
-};
-
-////////////////////////////////////////////////////////////////////////////////
+}
